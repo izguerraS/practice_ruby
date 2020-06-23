074172aba564626e16b06e10a883906391641a7d
“YOUR <Izguerra.sam@gmail.com>
2020-06-22 20:26:50 -0500

    changes made

---
+++ store_item.rb
@@ -0,0 +1,14 @@
+# c.1 = {chips: "BBQ", soda: 2.50, bread: "rye"}
+# c.2 = {"candy" => "twix", candy: "snickers", candy: "MM" }
+# c.3 = 
+
+
+#class
+
+class Employee
+    def initialize
+        p "I am in the initilize method"
+    end
+end
+
+Employee1 = Employee.new
