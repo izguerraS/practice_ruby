d32693454c244799d562d52cbe8426537bfbdb31
Sam <Izguerra.sam@gmail.com>
2020-06-24 10:12:37 -0500

    first attempt at loop

--- manager.rb
+++ manager.rb
@@ -1,12 +1,13 @@
 class Employee
-    attr_reader :first_name, :last_name, :active
-    attr_writer :active
+    attr_reader :first_name, :last_name, :active, :give_all_raises
+    attr_writer :active, :give_all_raises
   
     def initialize(input_options)
       @first_name = input_options[:first_name]
       @last_name = input_options[:last_name]
       @salary = input_options[:salary]
       @active = input_options[:active]
+      @give_all_raises = input_options[:give_all_raises]
     end
   
     def print_info
@@ -24,8 +25,8 @@ class Employee
   employee2.print_info
   
   class Manager < Employee
-    attr_reader :send_report
-    attr_writer :send_report
+attr_reader :give_all_raise, :send_report, 
+attr_writer :give_all_raises, :send_report, 
 
     def initialize(input_options)
       super
@@ -34,11 +35,11 @@ class Employee
 
     def give_all_raises
         i = 0
-        while i < :employee.length
-            @salary = 1.05 * @salary
+        while i < :employees.length
+            @give_all_raises = give_annual_raise
             i += 1
         end
-
+        p :employees.give_all_raises
   
     def send_report
       puts "Sending email..."
@@ -51,4 +52,4 @@ end
   manager = Manager.new(first_name: "Saron", last_name: "Yitbarek", salary: 100000, active: true, employees: [employee1, employee2])
   #manager.print_info
   #manager.send_report
-  manager.give_all_raises
+  #manager.give_all_raises
