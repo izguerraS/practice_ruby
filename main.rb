d205015ef242a4bce11009de0faa6246d7734837
Sam <Izguerra.sam@gmail.com>
2020-06-24 21:02:06 -0500

    whirlwind hashes start

--- main.rb
+++ main.rb
@@ -2,3 +2,4 @@ require_relative 'bike_class.rb'
 require_relative 'class_car.rb'
 require_relative 'vehicle_class.rb'
 
+bikes = bike.new
--- module_example.rb
+++ module_example.rb
@@ -1,6 +1,3 @@
-module Driveable
-
-
 class Car
     def initialize
       @speed = 0
@@ -24,26 +21,4 @@ class Car
     end
   end
   
-  class Bike
-        
-    def initialize
-    super
-    @
-    end
   
-    def brake
-      @speed = 0
-    end
-  
-    def accelerate
-      @speed += 10
-    end
-  
-    def turn(new_direction)
-      @direction = new_direction
-    end
-  
-    def ring_bell
-      puts "Ring ring!"
-    end
-  end
